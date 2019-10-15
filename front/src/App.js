33a381477fb4ccbc18a83056ab59e20b5b34d738
Henry Chen <henrychen385@gmail.com>
2019-10-15 23:08:13 +0800

    changed it to an es6 function

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div>
