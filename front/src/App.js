711ae5d885916608793f07a58231566d913624fe
jumper385 <henrychen385@gmail.com>
2019-10-15 20:40:15 +0800

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <p>The Team</p>
+        <p>The Car</p>
+        <p>Photo Gallery</p>
+        <p>About Us</p>
+        <p>Contact Us</p>
+      </div>
+      <h1>UWA MOTOR SPORTS</h1>
+      <p>Random Subtitle - WOW! Inspiring!!!</p>
+    </div>
+  );
+}
+
+export default App;
