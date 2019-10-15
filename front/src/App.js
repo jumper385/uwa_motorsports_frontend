ac32a4fa8e4d4e585083275e4b73f46363604a5f
Henry Chen <henrychen385@gmail.com>
2019-10-15 22:23:33 +0800

    routing implementations

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,59 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Router>
       <div>
-        <p>The Team</p>
-        <p>The Car</p>
-        <p>Photo Gallery</p>
-        <p>About Us</p>
-        <p>Contact Us</p>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/team'>The Team</Link>
+          <Link to='/car'>The Car</Link>
+          <Link to='/gallery'>Gallery</Link>
+          <Link to='/contact'>Contact Us</Link>
+        </nav>
+
+        <Switch>
+          <Route exact path='/'><Home/></Route>
+          <Route path='/team'><Team /></Route>
+          <Route path='/car'><Car /></Route>
+          <Route path='/gallery'><Gallery /></Route>
+          <Route path='/contact'><Contact /></Route>
+        </Switch>
+
       </div>
-      <h1>UWA MOTOR SPORTS</h1>
-      <p>Random Subtitle - WOW! Inspiring!!!</p>
-    </div>
+    </Router>    
   );
+}
+
+const Home = () => {
+  return(
+    <h1>Home Page - UWA Motor Sports 2019</h1>
+  )
+}
+
+const Team = () => {
+  return(
+    <h1>The Team</h1>
+  )
+}
+
+const Car = () => {
+  return(
+    <h1>The Car</h1>
+  )
+}
+
+const Gallery = () => {
+  return(
+    <h1>Gallery</h1>
+  )
+}
+
+const Contact = () => {
+  return(
+    <h1>Contact Us</h1>
+  )
 }
 
 export default App;
